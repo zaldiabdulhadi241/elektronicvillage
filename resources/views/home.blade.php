aea8de4bc79845824374003e1d355a6a8bdcc057
zaldiabdulhadi <zaldiabdulhadi12@gmail.com>
2023-12-01 13:09:50 +0700

    third-commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,70 @@
                     </a>
                 </div>
                 <div class="w-80 mt-4 h-1 mb-10 bg-primary"></div>
-                <div class="grid grid-cols-4">
+                <div class="grid grid-cols-4 gap-x-4">
+                    <a href="#" class="bg-white border border-border rounded-3xl shadow relative overflow-hidden">
+                        <div
+                            class="w-16 h-16 bg-primary absolute top-0 right-0 rounded-bl-3xl flex flex-col justify-center items-center text-bg3 font-bold">
+                            <p class="text-sm">56%</p>
+                            <p class="text-sm">OFF</p>
+                        </div>
+                        <div class="bg-bg1">
+                            <img class="rounded-t-lg mx-auto" width="150px" src="{{ asset('images/produk1.png') }}"
+                                alt="" />
+                        </div>
+                        <div class="p-5">
+                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Galaxy S22 Ultra
+                            </h5>
+                            <div class="with-discount flex flex-row gap-x-2 items-center">
+                                <p class="mb-3 font-bold text-lg">Rp19.800.000</p>
+                                <p class="mb-3 font-bold text-md line-through text-light">Rp20.000.000</p>
+                            </div>
+                            <hr class="text-border mb-2">
+                            <p class="mb-3 font-normal text-primary text-md">Hemat - Rp200.000</p>
+                        </div>
+                    </a>
+                    <a href="#" class="bg-white border border-border rounded-3xl shadow relative overflow-hidden">
+                        <div
+                            class="w-16 h-16 bg-primary absolute top-0 right-0 rounded-bl-3xl flex flex-col justify-center items-center text-bg3 font-bold">
+                            <p class="text-sm">56%</p>
+                            <p class="text-sm">OFF</p>
+                        </div>
+                        <div class="bg-bg1">
+                            <img class="rounded-t-lg mx-auto" width="150px" src="{{ asset('images/produk1.png') }}"
+                                alt="" />
+                        </div>
+                        <div class="p-5">
+                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Galaxy S22 Ultra
+                            </h5>
+                            <div class="with-discount flex flex-row gap-x-2 items-center">
+                                <p class="mb-3 font-bold text-lg">Rp19.800.000</p>
+                                <p class="mb-3 font-bold text-md line-through text-light">Rp20.000.000</p>
+                            </div>
+                            <hr class="text-border mb-2">
+                            <p class="mb-3 font-normal text-primary text-md">Hemat - Rp200.000</p>
+                        </div>
+                    </a>
+                    <a href="#" class="bg-white border border-border rounded-3xl shadow relative overflow-hidden">
+                        <div
+                            class="w-16 h-16 bg-primary absolute top-0 right-0 rounded-bl-3xl flex flex-col justify-center items-center text-bg3 font-bold">
+                            <p class="text-sm">56%</p>
+                            <p class="text-sm">OFF</p>
+                        </div>
+                        <div class="bg-bg1">
+                            <img class="rounded-t-lg mx-auto" width="150px" src="{{ asset('images/produk1.png') }}"
+                                alt="" />
+                        </div>
+                        <div class="p-5">
+                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Galaxy S22 Ultra
+                            </h5>
+                            <div class="with-discount flex flex-row gap-x-2 items-center">
+                                <p class="mb-3 font-bold text-lg">Rp19.800.000</p>
+                                <p class="mb-3 font-bold text-md line-through text-light">Rp20.000.000</p>
+                            </div>
+                            <hr class="text-border mb-2">
+                            <p class="mb-3 font-normal text-primary text-md">Hemat - Rp200.000</p>
+                        </div>
+                    </a>
                     <a href="#" class="bg-white border border-border rounded-3xl shadow relative overflow-hidden">
                         <div
                             class="w-16 h-16 bg-primary absolute top-0 right-0 rounded-bl-3xl flex flex-col justify-center items-center text-bg3 font-bold">
@@ -101,7 +164,8 @@
                                 alt="" />
                         </div>
                         <div class="p-5">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Galaxy S22 Ultra
+                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Galaxy S22
+                                Ultra
                             </h5>
                             <p class="mb-3 font-bold text-lg">Rp19.800.000</p>
                             <hr class="text-border mb-2">
